e965b8179b2653155ac4fc59e4b84426b2d1c28c
h8ktnkgck4-cpu <h8ktnkgck4@privaterelay.appleid.com>
2026-09-22 19:20:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zz-v1';
+const CACHE = 'zz-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
